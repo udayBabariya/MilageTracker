0e7b6defd36242c2747239ff9379b2a6a0466070
uday <uday@mobifyi.com>
2021-08-25 06:28:08 +0530

    feat(MotionTracking) - fetch information from motion tracker and show state of user's activity

--- MilageTracker/ViewController.swift
+++ MilageTracker/ViewController.swift
@@ -6,12 +6,49 @@
 //
 
 import UIKit
+import FirebaseAnalytics
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    //MARK: Outlets
+    @IBOutlet weak var activityStatusLabel: UILabel!
+    
+    //MARK: Constants:-
+    let activityManager = CMMotionActivityManager()
+    let padoMeter = CMPedometer()
 
+    //MARK:-LifeCycle:-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        Analytics.logEvent("AppStart", parameters: [:])
+        setUpActivityManager()
+    }
+    
+    
+    /// start tracking activity if feature is available
+    func setUpActivityManager(){
+        if CMMotionActivityManager.isActivityAvailable(){
+            self.activityManager.startActivityUpdates(to: .main) { data in
+                DispatchQueue.main.async {
+                    if let activity = data{
+                        if activity.running{
+                            self.activityStatusLabel.text = "Running"
+                            print("Runnnnnning")
+                        }else if activity.stationary{
+                            self.activityStatusLabel.text = "Stoped"
+                            print("Stoped")
+                        }else if activity.walking{
+                            self.activityStatusLabel.text = "walking"
+                            print("walking")
+                        }else if activity.automotive{
+                            self.activityStatusLabel.text = "Automobile"
+                            print("Automobile")
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
